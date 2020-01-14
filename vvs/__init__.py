1a890d8be052cee6cbeda287e99a2bc6098ca61f
pablo <pjcalvov@gmail.com>
2020-01-14 15:33:36 +0100

    more config

--- vvs/__init__.py
+++ vvs/__init__.py
@@ -1,5 +1,12 @@
 from dynaconf import settings
+import os
+import json
 
 app_settings = {
     'ENV': settings.ENV_FOR_DYNACONF,
 }
+
+# create google credentials variable
+credentials = json.loads(os.environ['GOOGLE_CREDENTIALS'])
+with open("credentials.json", 'w') as f:
+    json.dump(credentials, f)
